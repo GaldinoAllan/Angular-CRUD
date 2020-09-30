4bdf80526ca7c568d07d900be74a1366f89355ad
Allan Galdino <allan.g.galdino@outlook.com>
2020-09-30 16:18:32 -0300

    view created

--- src/app/post/view/view.component.ts
+++ src/app/post/view/view.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { Post } from '../post';
+import { PostService } from '../post.service';
 
 @Component({
   selector: 'app-view',
@@ -7,9 +11,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewComponent implements OnInit {
 
-  constructor() { }
+  id: number;
+  post: Post;
+
+  constructor(
+    public postService: PostService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.params['postId'];
+
+    this.postService.find(this.id).subscribe((data: Post) => {
+      this.post = data;
+    })
   }
 
 }
